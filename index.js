4497faae7aefeaa020bcfcf6d6050c4f1e6b6b5a
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-05-07 18:47:03 +0500

    node.js es6 module export and import

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const {genRandNum, celsiusToFarenheit} = require('./util');
-// const genRandNum = require('./util');
-// const celsiusToFarenheit = require('./util');
+// * ES6 module
+import { getPosts } from "./postController.js";
 
-console.log(`Random Number is : ${genRandNum()}`);
-console.log(`50 Celsius to farhenheit is : ${celsiusToFarenheit(50)}`);
+const myPosts = getPosts();
+
+console.log(myPosts[0].title);
+
+
+// * commonJS Module
+// const {genRandNum, celsiusToFarenheit} = require('./util');
+// // const genRandNum = require('./util');
+// // const celsiusToFarenheit = require('./util');
+
+// console.log(`Random Number is : ${genRandNum()}`);
+// console.log(`50 Celsius to farhenheit is : ${celsiusToFarenheit(0)}`);
